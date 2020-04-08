c11527b0341f81f5c9a301a706e24995145a3c9d
Amy Simpson <simpson.r.amy@gmail.com>
2020-04-08 20:25:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
   delete object[key]
 }
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object; 
+}
